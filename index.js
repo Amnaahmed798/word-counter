ac985decd1c19617ad167df572a149701a2553b0
Amna Ahmed <160534192+Amnaahmed798@users.noreply.github.com>
2024-04-17 00:18:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "create a sentence to count the word: ",
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+//print the error of words to the console
+console.log(words);
+//print the word count of the sentence to the console
+console.log(`your sentence word count is ${words.length}`);
